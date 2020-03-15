d253f05bdea8cb6ed4cf00878e99d3d5bd814947
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-03-15 10:21:53 +0530

    order success tab

---
+++ app/src/main/java/com/args/kcnagro/Order_Sucess_activity.java
@@ -0,0 +1,14 @@
+package com.args.kcnagro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Order_Sucess_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order__sucess_activity);
+    }
+}
